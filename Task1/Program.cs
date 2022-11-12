5f349ab862bc3a52d8ec7cbef3e54fc6a8463055
MariiaGumenyk <sana22_gmd@student.ztu.edu.ua>
2022-11-12 16:24:00 +0200

    3 lines changed

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -1,7 +1,8 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Курси Sana Commerce Software Development Bootcamp");
-Console.WriteLine("Курси Sana Commerce Software Development Bootcamp");
-Console.WriteLine("Курси Sana Commerce Software Development Bootcamp");
+Console.WriteLine("Курси Sana Commerce Software Development Bootcamp text 1");
+Console.WriteLine("Курси Sana Commerce Software Development Bootcamp text 1");
+Console.WriteLine("Курси Sana Commerce Software Development Bootcamp text 1");
+Console.WriteLine("Курси Sana Commerce Software Development Bootcamp ");
 Console.WriteLine("Курси Sana Commerce Software Development Bootcamp");
 Console.WriteLine("Курси Sana Commerce Software Development Bootcamp");
 Console.WriteLine("Курси Sana Commerce Software Development Bootcamp");
